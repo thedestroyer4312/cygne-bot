e702221264e8fcf107ac2cac5732fc71d3aa4084
Trevor Tsai <trevortsai1@gmail.com>
2022-03-02 23:50:12 -0800

    Working on getting initial authorization working

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
 
-function init() {
-  return 0;
-}
+// OAuth2
+import { exchangeCode } from "./oauth2.js";
 
-function main() {
-  return 0;
-}
+const app = express();
 
-init();
-main();
+// Init variables from .env
+export const PORT = process.env.PORT || "3001";
+export const CLIENT_ID = process.env.DISCORD_OAUTH2_CLIENT_ID;
+export const CLIENT_SECRET = process.env.DISCORD_OAUTH2_CLIENT_ID;
+
+// Redirect URI
+export const REDIRECT_URI_BASE = process.env.REDIRECT_URI_BASE || `http://localhost:${PORT}`;
+export const REDIRECT_URI_ENDPOINT = "/api/auth/discord/redirect";
+
+// Discord URLS
+export const DISCORD_URL_BASE = "https://discord.com";
+export const DISCORD_BASE_AUTHORIZATION_ENDPOINT = "/api/oauth2/authorize";
+export const DISCORD_TOKEN_ENDPOINT = "/api/oauth2/token";
+export const DISCORD_TOKEN_REVOCATION_ENDPOINT = "/api/oauth2/token/revoke";
+
+/**
+ * @description When redirected to redirect URI, retrieves the authorization code from the user
+ * and attempts to exchange for access token/refresh token pair
+ */
+app.get(REDIRECT_URI_ENDPOINT, (req, res) => {
+  console.log(req);
+
+  // Retrieve code from request
+
+  const { code } = req.query;
+
+  // If code exists, exchange code for token
+  // Otherwise, bad request - 400
+  if (code) {
+    exchangeCode(code.toString())
+      .then((data) => {
+        console.log(data);
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(400);
+      });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+// Start listening on given port
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
---
+++ util.js
@@ -0,0 +1,42 @@
+import https from "https";
+
+/**
+ * @function httpsRequest
+ * @param {RequestOptions} params
+ * @param {string} postData
+ * @return {Promise<ClientRequest>} Promisifed request
+ */
+export function httpsRequest(params, postData = null) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(params, (res) => {
+      // Reject on bad status
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        return reject(new Error(`statusCode=${res.statusCode}`));
+      }
+
+      // Accumulate data (res is a readable stream)
+      let body = [];
+      res.on("data", (chunk) => body.push(chunk));
+
+      // On finish, attempt to parse body into JSON object
+      res.on("end", () => {
+        try {
+          body = JSON.parse(Buffer.concat(body).toString());
+        } catch (err) {
+          reject(err);
+        }
+        resolve(body);
+      });
+    });
+
+    // If request error, reject
+    req.on("error", (err) => reject(err));
+
+    // If there is POST data, then write it to the request
+    if (postData) {
+      req.write(postData);
+    }
+
+    req.end();
+  });
+}
